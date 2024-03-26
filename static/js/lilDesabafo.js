1348f3f4cad7b8f96bff10159b1a60788c316cc2
thCoders2 <137647343+thCoders2@users.noreply.github.com>
2024-03-25 22:36:42 -0300

    instruções static/js/lilDesabafo.js

---
+++ static/js/lilDesabafo.js
@@ -0,0 +1,7 @@
+// Instruções para lilDesabafo.js
+
+// devera salvar as informações com localStorage prefixo padrão 'lilDesabafo-' por exemplo: 
+// lilDesabafo-desabafo -> desabafo um json assim => {'desabafo':'Um desabafinho para usar como exemplo, não espere muito de mim'}
+// lilDesabafo-autor -> json autor -> autor = JSON.parse(localStorage.getItem('lilDesabafo-autorKarminha', None)) => {'autor':'karminha', 'cor1':'#f00', 'cor2':'#AAAAFF', 'desabafos':['first','desabei :3']}
+// lilDesabafo-autores -> array de autores -> sendo possivel adicionar, ou remover autores e até baixar autores
+// lilDesabafo-database -> é importante que a database seja possivel fazer o download, com funções deve ter um botão de download para salvar os desabafos
